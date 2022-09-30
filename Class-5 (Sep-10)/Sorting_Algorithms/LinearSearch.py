535eac68113b4a24c4067c14aa59555522c0fe01
Dikshant <guptadikshant99@gmail.com>
2022-09-30 15:33:52 +0530

    Updating the directory changes

--- Class-5 (Sep-10)/Sorting_Algorithms/LinearSearch.py
+++
@@ -1,9 +0,0 @@
-def search(arr, ele):
-	for i in range(len(arr)):
-		if arr[i] == ele:
-			return i
-	else:
-		return -1
-
-if __name__ == "__main__":
-	print(search(arr=[20,45,27, 47,55,67,75,88,90],ele=95))
