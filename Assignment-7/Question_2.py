f1998e2dc7ad7e19ef4539c6dc09d0b8bb7e1953
Dikshant <guptadikshant99@gmail.com>
2022-10-03 00:11:17 +0530

    Added problem statement

--- Assignment-7/Question_2.py
+++ Assignment-7/Question_2.py
@@ -1,4 +1,12 @@
 from typing import List
+"""
+Given three points, check whether they lie on a straight (collinear) or not. [Google]
+For example:
+Input- [(1,1), (1,6), (0,9)]
+Output- No
+Input- [(1,1), (1,4), (1,5)]
+Output- Yes
+"""
 
 """
 The Time complexity of this solution is O(1)
