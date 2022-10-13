7392b81ed8d4722e82d1040bde13a16ef5e07304
Dikshant Gupta <guptadikshant99@gmail.com>
2022-10-13 09:12:34 +0530

    Updated comments of Question-1

--- Assignment-8/Question-1.py
+++ Assignment-8/Question-1.py
@@ -7,9 +7,19 @@ For example:
 arr = [40, 25, 68, 79, 52, 66, 89, 97]
 k = 2
 result = 40
+"""
 
+"""
+Approach of the solution : 
+    1) First sort the array in ascending order
+    2) Running loop K times so that we can pop out the elements that many number of times
+        and will give the kth smallest element
+"""
+
+"""
 Total time complexity of this solution is O(k+N log N)
 """
+
 
 from typing import List
 import heapq
@@ -17,13 +27,8 @@ import heapq
 
 def kSmallestElement(arr: List[int], k) -> int:
 
-    #First sort the array in ascending order
-
     min_heap = sorted(arr) # time complexity O(N logN)
     result :int = 0
-
-    # Running loop K times so that we can pop out the elements that many number of times
-    # and will give the kth smallest element
 
     for i in range(k): # time complexity O(k)
         result = heapq.heappop(min_heap)
