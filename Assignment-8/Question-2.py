e7d6f3790f58e6cfe31c9dbefc0e8b4149f86f13
Dikshant Gupta <guptadikshant99@gmail.com>
2022-10-13 09:09:24 +0530

    Committing solution of Question-2 of Assignment-8

--- Assignment-8/Question-2.py
+++ Assignment-8/Question-2.py
@@ -1,10 +1,49 @@
+"""
+Given array nums with n objects colored red, white, or blue, sort them in place so that
+the objects of the same color are adjacent, with the colors in the order red, white, and
+blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue
+respectively.
+Solve this question without using the library sort function.
+For example:
+Nums = [2,0,2,1,1,0]
+Result = [0,0,1,1,2,2]
+"""
+
+"""
+Approach of the solution : We can use Quick Sort for this problem as it has O(N log(N)) time complexity
+"""
+
 from typing import List
-import heapq
 
+def sortColors(nums: List[int]) -> List[int]:
+    p = 0
+    q = len(nums) - 1
+    def partition(nums, p, q):
 
-def sortColors(nums:List[int]) -> List[int]:
+        pivot = nums[p]
+        i = p
 
-    pass
+        for j in range(i + 1, q+1):
+            if nums[j] <= pivot:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i], nums[p] = nums[p], nums[i]
+
+        return i
+
+    def quickSort(nums, p, q):
+
+        if p < q:
+            mid = partition(nums, p, q)
+            quickSort(nums, p, mid-1)
+            quickSort(nums, mid+1, q)
+
+        return nums
+
+    sorted_nums = quickSort(nums,p,q)
+
+    return sorted_nums
 
 
 if __name__ == "__main__":
