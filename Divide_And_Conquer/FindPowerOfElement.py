2ac09b52f6ff398b4928fda72d5686591c51b366
Dikshant Gupta <guptadikshant99@gmail.com>
2022-10-16 23:16:15 +0530

    Adde Second Approach

--- Divide_And_Conquer/FindPowerOfElement.py
+++ Divide_And_Conquer/FindPowerOfElement.py
@@ -24,3 +24,30 @@ if __name__ == "__main__":
     result = FindPowerOfElement(num, power)
 
     print(f"The result is : {result}")
+
+
+
+# Second Approach
+def pow(a,b):
+
+    if b == 0:
+        return 1
+
+    if b % 2 ==0:
+        return pow(a, b//2) * pow(a, b//2)
+
+    else:
+        return a * pow(a, b//2) * pow(a, b//2)
+
+
+
+def myPow(a , b):
+
+    x = pow(a,b)
+
+    if b < 0:
+        print(1/x)
+    else:
+        print(x)
+
+myPow(2,-2)
