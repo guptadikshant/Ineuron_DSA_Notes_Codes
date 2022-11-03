4ee3555ad41365378d158c6574ca74d6aaa9b71b
Dikshant Gupta <guptadikshant99@gmail.com>
2022-11-03 23:07:08 +0530

    counting of nodes inside linkded list

---
+++ Linked_List/Count/Counting.py
@@ -0,0 +1,56 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.nextPtr = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    ## insert the new node at the end of the linked list
+    def insertAtEnd(self, new_data):
+        ## create a new node for the new data
+        new_node = Node(new_data)
+
+        ## check whether linked list is empty or different
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        ## insert the data at the end
+        temp = self.head
+        while temp.nextPtr:
+            temp = temp.nextPtr
+    
+        temp.nextPtr = new_node
+
+    def countNodes(self):
+        temp = self.head
+        count = 0
+        while temp:
+            temp = temp.nextPtr
+            count += 1
+        return count
+
+    def printLinkedList(self):
+        temp = self.head
+        while temp:
+            print(str(temp.data)+" ", end=" ")
+            temp = temp.nextPtr
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+
+    llist.insertAtEnd(10)
+    llist.insertAtEnd(20)
+    llist.insertAtEnd(30)
+    llist.insertAtEnd(40)
+    llist.insertAtEnd(50)
+    print("Insertion of nodes at the end of the Linked List:")
+    llist.printLinkedList()
+    print()
+    count_of_nodes = llist.countNodes()
+    print(f"Number of nodes in Linked List are : {count_of_nodes}")
